0709fcdf0ed3b426ba0b8e6ba521d07f17248a61
FryderykG <eszkimos@gmail.com>
2017-11-29 19:41:09 +0100

    add Layout component and Higher-OrderComponent Aux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <Layout>
+        <p>test</p>
+      </Layout>
     );
   }
 }
+
 
 export default App;
