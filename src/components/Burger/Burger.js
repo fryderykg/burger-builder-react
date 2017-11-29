72dcdc449b53507a51fe8b9e7466e8e184529f0b
FryderykG <eszkimos@gmail.com>
2017-11-29 20:57:51 +0100

    dynamic build burger passing props from BurgerBuilder to Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './burger.css';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient type={ingKey} key={ingKey + i}/>
+      })
+    });
+
+  console.log(props.ingredients['meat']);
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
