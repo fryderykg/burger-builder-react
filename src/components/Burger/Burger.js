0ba18896885baea7cb5fb55bc317e81bf40686fb
FryderykG <eszkimos@gmail.com>
2017-11-29 21:09:30 +0100

    check if no ingredients, some correction in Burger styles

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './burger.css';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingKey => {
       return [...Array(props.ingredients[ingKey])].map((_, i) => {
         return <BurgerIngredient type={ingKey} key={ingKey + i}/>
       })
-    });
+    })
+    .reduce((arr, el) => {
+    return arr.concat(el)
+    }, []);
 
-  console.log(props.ingredients['meat']);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
 
   return (
     <div className={styles.Burger}>
