8fd840b8528a298c8975ee57a5c7d0f2c9e7cf3a
FryderykG <eszkimos@gmail.com>
2017-12-03 17:27:55 +0100

    add loader do OrderSummary while data is loading to server

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,7 +9,10 @@ const style = classNames.bind(styles);
 class Modal extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show
+    return (
+      nextProps.show !== this.props.show
+      || nextProps.children !== this.props.children
+    )
   }
 
   render() {
---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './spinner.css';
+
+const spinner = (props) => {
+  return (
+    <div className={styles.Loader}>Loading...</div>
+  );
+};
+
+export default spinner;
