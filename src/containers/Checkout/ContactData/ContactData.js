8eb4ea7830e974c7e214702f5f8d4ded3f3ac75b
FryderykG <eszkimos@gmail.com>
2017-12-10 16:09:00 +0100

    add Input component, in ContactData replace input element with this new component.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import axios from '../../../axios-orders';
 
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Button from '../../../components/UI/Button/Button';
+import Input from '../../../components/UI/Input/Input';
 
 
 import Styles from './contactData.css';
@@ -54,10 +55,10 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input type="text" name="name" placeholder="Your name"/>
-        <input type="email" name="email" placeholder="Your email"/>
-        <input type="text" name="street" placeholder="Street"/>
-        <input type="text" name="postal" placeholder="Postal Code"/>
+        <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
+        <Input inputtype="input" type="email" name="email" placeholder="Your email"/>
+        <Input inputtype="input" type="text" name="street" placeholder="Street"/>
+        <Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
